dc15f2e21ca5ffa704a2b7e9b16f37e642f82038
weixiao <weixiao@megvii.com>
2021-07-13 20:11:20 +0800

    fix: 添加具体的size分析, 部分复合属性的拆分

--- scss-trans/config-template.js
+++ scss-trans/config-template.js
@@ -32,20 +32,25 @@ module.exports = {
     autograph: '!===<css>===!    添加标识,方便检索脚本转换的css文件',
     // 跟页面font-size,用于px to rem计算
     rem: 16,
-    // 最小rem步长，tailwind目前rem的最小步长为0.125rem
-    remStep: 0.125,
+    // number值转换为tailwind相关的number, 参数 (key: String, value: Number, rem): Number
+    // numberTransFunc: Function,
+    // px to rem 转换方法，参数 (key: String, value: Number, rem): Number
+    // remTransFunc: Function,
     // 计算百分比时，暴露的小数点后位数
     percentFixNum: 6,
     // 是否强行将颜色转换为相近的tailwind颜色
-    approxColor: true,
+    // 目前分析的规则和志刚定的规则不一致，暂时设为false
+    approxColor: false,
   },
   file: {
     // 需要转换的文件夹路径
     from: todoList.map(name => `./components/${name}`),
     // 转换的文件类型
-    include: [/\.scss$/],
+    include: [/index\.scss$/],
     // 若to是相对路径，则相对于检测的文件以to路径进行输出
     // 若to是绝对路径，to即为输出路径
     to: './opt-[name].css',
   },
+  // todo::: 后续应支持tailwind的拓展配置
+  // extend: Object
 };
